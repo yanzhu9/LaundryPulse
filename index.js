97b52b1ac8c72f687c2bc5a2825ed6c4098b1c46
yanzhu9 <E1513114@u.nus.edu>
2026-05-28 17:02:15 +0800

    to insert data in booking table

--- index.js
+++ index.js
@@ -178,7 +178,6 @@ app.post("/api/queue-book", async (req, res) => {
         {
           user_id: user_id,
           machine_id: targetMachine.machine_id,
-          machine_type: type,
           booking_status: "using"
         }
       ]);
@@ -199,7 +198,6 @@ app.post("/api/queue-book", async (req, res) => {
       await supabase.from("Booking_Table").insert([
         {
           user_id: user_id,
-          machine_type: type,
           booking_status: "waiting"
         }
       ]);
