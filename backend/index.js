95b7af5bb1282dc5b7feab1abe4fac73009255b3
yanzhu9 <E1513114@u.nus.edu>
2026-07-08 10:10:07 +0800

    improve register

--- backend/index.js
+++ backend/index.js
@@ -108,7 +108,7 @@ app.post('/register', async (req, res) => {
 
     await supabase
       .from('User_Table')
-      .insert([{ email, password, credit_score: 15}]);
+      .insert([{ email, password, credit_score: 15, role: "user"}]);
 
     return res.json({
       success: true,
